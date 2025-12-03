988fc5cf7cd8ad84e3ac5803589f5e2e1c5760d9
ismanby <ismaanby@gmail.com>
2025-12-03 17:27:51 +0700

    Modifikasi Percobaan 2 - Pertanyaan No. 2

--- Percobaan2.java
+++ Percobaan2.java
@@ -4,7 +4,7 @@ public class Percobaan2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int bilangan, pangkat;
+        int bilangan, pangkat, hasil;
 
         System.out.print("Bilangan yang dihitung: ");
         bilangan = sc.nextInt();
@@ -12,15 +12,19 @@ public class Percobaan2 {
         System.out.print("Pangkat: ");
         pangkat = sc.nextInt();
 
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        System.out.print("Proses: ");
+        hasil = hitungPangkat(bilangan, pangkat);
+        System.out.println(" = " + hasil);
         
         sc.close();
     }
 
     static int hitungPangkat(int x, int y) {
         if (y == 0) {
+            System.out.print("1");
             return(1);
         } else {
+            System.out.print(x + " x ");
             return(x * hitungPangkat(x, y - 1));
         }
     }
